5f66ef67656d5cf8e8768c33ede0de8982bcc070
Brian McClain <brianmmcclain@gmail.com>
2019-03-06 12:22:23 -0500

    Add some logging, log to stdout without buffering

--- app.rb
+++ app.rb
@@ -5,15 +5,16 @@ require 'net/http'
 require 'httparty'
 
 before do
+    $stdout.sync = true
     @routerDomain = ENV["ROUTER_DOMAIN"]
 end
 
 def dispatch(intentName, params)
     uri = "http://#{intentName.downcase}.#{@routerDomain}"
     headers = {"Content-Type": "application/json"}
-    puts uri
-    puts headers
+    puts "Invoking action URI: #{uri}"
     response = HTTParty.post(uri, :headers => headers, :body => params.to_json)
+    puts "ACTION RESPONSE: #{response.body}"
     return build_response(response.body)
 end
 
@@ -23,6 +24,7 @@ end
 
 post '/' do
     body = request.body.read
+    puts body
     jBody = JSON.parse(body)
     return dispatch(jBody["queryResult"]["intent"]["displayName"], jBody["queryResult"]["parameters"])
 end
